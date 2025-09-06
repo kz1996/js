04161865e017f5b4703dee1e5971aa81e5640065
kz1996 <zkun0172@gmail.com>
2025-09-06 01:05:30 -0700

    Add files via upload

--- lecture2/hw3.js
+++ lecture2/hw3.js
@@ -2,22 +2,12 @@
 // Explain your answer.
 
 console.log(0.1 + 0.2);
-0.30000000000000004
-Because js use IEEE754 double-precision floating point values to stored. so the output will very close to 0.3 but will not === 0.3.
 
 console.log(0.1 + 0.2 == 0.3);
-false
-== will compare type and value
-
 
 console.log(1 +  "2" + "2");
-122
-1+"2" goes to "12" js will covent the number1 to shring'1'
-'12'+'2' is two string merage.
-
 
 console.log(1 +  +"2" + "2");
-
 
 console.log(1 +  -"1" + "2");
 
