6d942b627ebb10f27dbed6104513f42b5c178611
kz1996 <zkun0172@gmail.com>
2025-09-08 11:05:15 -0700

    Update hw1.js

--- lecture2/hw1.js
+++ lecture2/hw1.js
@@ -3,21 +3,31 @@
 * If o and p have a property by the same name, o's property is overwritten.
 * This function does not handle getters and setters or copy attributes.
 */
+function extend(o, p) {
+for (let key in p){
+    if(object.prototype.hasOwnPropertie.call(p,key)){
+        o[key]=p[key];
+}
+}
+return o;
+}
 
 /*
 * Return a new object that holds the properties of both o and p.
 * If o and p have properties by the same name, the values from o are used.
 */
-function union(o, p) {
-    // implement your code here
-}
 
 /*
 * Remove properties from o if there is not a property with the same name in p.
 * Return o.
 */
 function restrict(o, p) {
-    // implement your code here
+    for (let key in o){
+        if(object.prototype.hasOwmPropertie.call(o,key)&& !(key in p)){
+            delete o[key];}
+}
+
+    return o;
 }
 
 /*
@@ -26,5 +36,10 @@ function restrict(o, p) {
 * the properties in p are discarded
 */
 function intersection(o, p) {
-    // implement your code here
+   const a={}
+    for (let key in o){
+        if(object.prototype.hasOwmPropertie.call(o,key)&& key in p)
+            a[key]=o[key];
+}
+    return a;
 }
